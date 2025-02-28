64d0cfb36bc5c55c145eea18e4b3741d32318204
alemalagni <alemalagni@gmail.com>
2025-02-28 23:38:31 +0100

    feat: JS add check

--- pari_e_dispari/js/app.js
+++ pari_e_dispari/js/app.js
@@ -2,5 +2,15 @@
 
 // Script
 const scelta = prompt("Scegliere tra pari e dispari");
-const numUser = parseInt(prompt("Scegliere un numero tra 1 e 5"));
+if ( scelta != pari || scelta != dispari ) {
+    console.log("Scelta non valida");
 
+} else {
+    const numUser = parseInt(prompt("Scegliere un numero tra 1 e 5"));
+
+    if ( numUser < 1 || numUser > 5 ) {
+        console.log("Scelta non valida");
+    } else {
+        
+    }
+}
